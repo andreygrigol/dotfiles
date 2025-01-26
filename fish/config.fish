a7830c5c1ccf5aea276af3bd7df7dab0ca841d3c
Andrey <andrey@a00c28f2b7e7>
2025-01-26 15:23:18 -0300

    Cleaning and adjusting

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,17 @@
-alias vi="nvim"
-alias ll="ls -l"
+starship init fish | source
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+#Aliases
+alias ls='ls --color=auto'
+alias ll='ls -l --color=auto'
+alias la='ls -la --color=auto'
+alias vi='nvim'
 
 if status is-interactive
     set -g fish_greeting
-    starship init fish | source
-    neofetch
+    fastfetch
+    # Commands to run in interactive sessions can go here
 end
+
+fish_add_path /home/andrey/.spicetify
