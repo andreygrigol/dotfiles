700df7b293cc4f1ac9781579fde9c219555d2e95
Andrey Ribeiro <aribeiro7@lenovo.com>
2025-02-06 13:25:36 -0300

    Created Kanagawa branch

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme vscode")
+vim.cmd("colorscheme kanagawa")
---
+++ nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,11 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+      undercurl = true,
+    }
+  end,
+}
