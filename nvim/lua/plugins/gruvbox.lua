f981658fbb24bb50410e49cc141d09b32735799d
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2025-02-17 13:07:54 -0300

    Updating neovim settings

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -5,6 +5,14 @@ return {
   opts = function()
     return {
       transparent_mode = true,
+      bold = true,
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      },
     }
   end,
 }
