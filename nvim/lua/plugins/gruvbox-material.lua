6d40f0522e4ec3ea04a8c4c216b782b4a4971094
Andrey Ribeiro <aribeiro7@lenovo.com>
2025-03-11 10:46:24 -0300

    Adjusted neovim for material theme and created new window binds for tmux

--- nvim/lua/plugins/gruvbox-material.lua
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -5,8 +5,8 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_foreground = "mix"
-    vim.g.gruvbox_material_background = "soft"
+    vim.g.gruvbox_material_foreground = "material"
+    vim.g.gruvbox_material_background = "mix"
     vim.g.gruvbox_material_enable_italic = true
     vim.cmd.colorscheme("gruvbox-material")
   end,
