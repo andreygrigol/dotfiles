de7f3664333949669f1f3e0b4611f00b0541aacd
Andrey <andrey@4d9d645ceee9>
2025-05-13 14:38:24 -0300

    Adding nvim variable path to fish and gruvbox dark soft theme for kitty

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@ alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 
+set -xg PATH $PATH /opt/nvim-linux-x86_64/bin/
+
 if status is-interactive
     set -g fish_greeting
     starship init fish | source
