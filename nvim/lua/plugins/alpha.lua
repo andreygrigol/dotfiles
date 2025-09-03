1d16287712b85b4991b29aada39d4a9753de4896
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2025-09-03 08:04:03 -0300

    Changed alpha's home page

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,10 +1,6 @@
 return {
   "goolord/alpha-nvim",
-  dependencies = {
-    "echasnovski/mini.icons",
-    "nvim-lua/plenary.nvim",
-  },
   config = function()
-    require("alpha").setup(require("alpha.themes.theta").config)
+    require("alpha").setup(require("alpha.themes.dashboard").config)
   end,
 }
