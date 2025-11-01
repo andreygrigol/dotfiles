6687f59d5d958cd79513f39ad070a012e5255cb9
Andrey <andrey.grigoletto2@gmail.com>
2025-11-01 09:47:14 -0300

    Adjusting fish and kitty configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
+alias la="ls -la"
+alias ls="ls -l"
+alias vi="nvim"
 
 set -xg PATH $PATH /opt/nvim-linux-x86_64/bin/
 
