0e3a0370fd00826fb4ee1b3bb902f994982db556
Andrey <andrey.grigoletto2@gmail.com>
2025-11-01 09:55:55 -0300

    Modifying gnome extensions and adding a new application to the environment: eza command

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,17 @@
+# Aliases
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
-alias la="ls -la"
-alias ls="ls -l"
+alias ls="eza"
+alias ll="eza -l"
+alias la="eza -la"
 alias vi="nvim"
+
+# Git aliases
+alias g="git"
+alias gst="git status"
+alias gcm="git commit -m"
+alias gpu="git push -u origin"
 
 set -xg PATH $PATH /opt/nvim-linux-x86_64/bin/
 
