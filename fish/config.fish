a123ea7723aaacb446ad6142ed4ee951416f364b
Andrey <andrey.grigoletto2@gmail.com>
2025-12-01 17:23:05 -0300

    Configuring extensions and a few files for fish, tmux and nvim

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,9 @@
-# Aliases
 alias grep="grep --color=auto"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
-alias ls="eza"
-alias ll="eza -l"
-alias la="eza -la"
+alias la="ls -la"
+alias ls="ls -l"
 alias vi="nvim"
-
-# Git aliases
-alias g="git"
-alias gst="git status"
-alias gcm="git commit -m"
-alias gpu="git push -u origin"
 
 set -xg PATH $PATH /opt/nvim-linux-x86_64/bin/
 
