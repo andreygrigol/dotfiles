21363186fefdf760ccf0df6801005db67ab6d473
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2026-01-08 10:28:40 -0300

    Fixing a bug of multiple windows in tmux and a few adjustments here and there

---
+++ nvim/lua/plugins/gruvbox-material.lua
@@ -0,0 +1,9 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = true,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_transparent_background = true
+    vim.g.gruvbox_material_foreground = "original"
+  end,
+}
