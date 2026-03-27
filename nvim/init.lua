8576a4c33573c85ce0d14a2dc24a617b46ee8783
Andrey Ribeiro <andrey.grigoletto2@gmail.com>
2026-03-27 08:17:26 -0300

    Somehow saved the treesitter problem?

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+
+callback = function()
+
+pcall(vim.treesitter.start)
+
+end,
+
+})
+
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,35 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-        highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = true,
-		},
-        indent = { enable = true },
-        ensure_installed = {
-            'lua',
-            'c',
-            'rust',
-            'python',
-			'html',
-			'css',
-			'typescript',
-        },
-        auto_install = true,
-    }
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  branch = 'main',
+  config = function()
+	local ts = require('nvim-treesitter')
+
+	local languages = {
+	  'lua', 'rust', 'python',
+	  'typescript', 'tsx', 'javascript', 'css',
+	  'regex', 'markdown',
+	}
+	ts.install(languages)
+
+	local ts_settings = vim.api.nvim_create_augroup('TSSettings', { clear = true })
+	vim.api.nvim_create_autocmd('FileType', {
+	  desc = 'Set up nvim-treesitter highlighting and indentation',
+	  group = ts_settings,
+	  pattern = languages,
+	  callback = function()
+		-- Syntax highlighting, provided by Neovim
+		vim.treesitter.start()
+		-- Indentation, provided by nvim-treesitter
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		-- Folds using treesitter expr, provided by Neovim
+		vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.wo[0][0].foldmethod = 'expr'
+	  end,
+	})
+	-- Start every file with folds open
+	vim.opt.foldlevel = 99
+	vim.opt.foldlevelstart = 99
+  end
 }
